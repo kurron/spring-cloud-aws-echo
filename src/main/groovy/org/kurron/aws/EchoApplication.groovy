06d5758e859ab9e70c9d7efd80e42b1068283825
Ron Kurr <kurr@kurron.org>
2017-02-10 15:19:33 -0500

    first commit

---
+++ src/main/groovy/org/kurron/aws/EchoApplication.groovy
@@ -0,0 +1,12 @@
+package org.kurron.aws
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class EchoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run EchoApplication, args
+	}
+}
