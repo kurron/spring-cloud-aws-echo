4319baa7b6d28b9194108bfe239b0d4c4c73869a
Ron Kurr <kurr@kurron.org>
2017-02-21 17:02:56 -0500

    Make the endpoint serve double duty.  If the correct headers and query parameters are specified, the endpoint will forward the request to another copy  of itself.  This is to showcase public and private load balancing.  This is what I used to test: http --verbose 127.0.0.1:8080 elb==localhost port==32768 endpoint==/ x-forwarded-host:111.222.333.444 x-forwarded-port:9090 x-forwarded-proto:https

--- src/main/groovy/org/kurron/aws/RestGateway.groovy
+++ src/main/groovy/org/kurron/aws/RestGateway.groovy
@@ -1,12 +1,13 @@
 package org.kurron.aws
 
 import groovy.transform.Memoized
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.http.*
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.client.RestOperations
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 
@@ -15,23 +16,56 @@ import java.time.Instant
 /**
  * Handles REST requests.
  */
+@SuppressWarnings( 'GroovyUnusedDeclaration' )
 @Controller
 class RestGateway {
 
+    /**
+     * The magic ip address that EC2 instance meta-data can be obtained from.
+     */
     private static String URL = 'http://169.254.169.254/latest/meta-data/hostname'
 
-    @RequestMapping( path = '/', method = [RequestMethod.GET], produces = ['application/json'] )
-    ResponseEntity<HypermediaControl> handleGet( UriComponentsBuilder builder, @RequestHeader Map<String, String> headers ) {
+    private final RestOperations theTemplate = new RestTemplateBuilder().setConnectTimeout( 1 ).setReadTimeout( 1 ).build()
 
-        String hostname = determineHostName( URL )
+    @GetMapping( path = '/', produces = ['application/json'] )
+    ResponseEntity<HypermediaControl> echoInstanceInformation( UriComponentsBuilder builder,
+                                                               @RequestHeader Map<String, String> headers,
+                                                               @RequestParam( required = false ) Optional<String> elb,
+                                                               @RequestParam( required = false ) Optional<Integer> port,
+                                                               @RequestParam( required = false ) Optional<String> endpoint ) {
 
-        def responseURL = builder.build().toUriString()
-        def control = new HypermediaControl( status: HttpStatus.OK.value(),
-                                             timestamp: Instant.now().toString(),
-                                             path: responseURL,
-                                             servedBy: hostname,
-                                             headers: headers )
+        // ugly but I want to do this quickly
+        final HypermediaControl control
+        if ( elb.present && port.present && endpoint.present ) {
+            control = constructPublicResponse( elb, port, endpoint, headers )
+        }
+        else {
+            control = constructPrivateResponse( builder, headers )
+        }
         ResponseEntity.ok( control )
+    }
+
+    private static HypermediaControl constructPublicResponse(Optional<String> elb, Optional<Integer> port, Optional<String> endpoint, Map<String, String> headers) {
+        def uri = UriComponentsBuilder.newInstance().scheme( 'http' ).host( elb.get() ).port( port.get() ).path( endpoint.get() ).build().toUri()
+        def template = RestTemplateBuilder.newInstance().build()
+        def httpHeaders = headers.inject( new HttpHeaders() ) { HttpHeaders accumulator, entry ->
+            // Spring does not respect the x-forwarded-for header so we need to map it to x-forwarded-host
+            accumulator.add( entry.key == 'x-forwarded-for' ? 'x-forwarded-host' : entry.key, entry.value )
+            accumulator
+        } as HttpHeaders
+        def request = new HttpEntity<String>( httpHeaders )
+        def response = template.exchange(uri, HttpMethod.GET, request, HypermediaControl)
+        response.body
+    }
+
+    private static HypermediaControl constructPrivateResponse( UriComponentsBuilder builder, Map<String, String> headers ) {
+        def hostname = determineHostName( URL )
+        def responseURL = builder.build().toUriString()
+        new HypermediaControl( status: HttpStatus.OK.value(),
+                               timestamp: Instant.now().toString(),
+                               path: responseURL,
+                               servedBy: hostname,
+                               headers: headers )
     }
 
     @Memoized
@@ -39,7 +73,8 @@ class RestGateway {
         final String hostname
         try {
             hostname = new RestTemplate().getForObject( url, String )
-        } catch ( Exception e ) {
+        }
+        catch ( Exception ignored) {
             hostname = 'Not Running In AWS'
         }
         hostname
