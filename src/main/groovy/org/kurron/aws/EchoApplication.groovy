39c3b027762a5852320f3cfc8f0abf750eb54233
Ron Kurr <kurr@kurron.org>
2017-03-18 13:13:19 -0400

    Added in X-Ray recorder.

--- src/main/groovy/org/kurron/aws/EchoApplication.groovy
+++ src/main/groovy/org/kurron/aws/EchoApplication.groovy
@@ -1,14 +1,37 @@
 package org.kurron.aws
 
+import com.amazonaws.xray.AWSXRay
+import com.amazonaws.xray.AWSXRayRecorderBuilder
+import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter
+import com.amazonaws.xray.plugins.EC2Plugin
+import com.amazonaws.xray.plugins.ECSPlugin
+import com.amazonaws.xray.strategy.sampling.LocalizedSamplingStrategy
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
 
+import javax.servlet.Filter
+
 @SpringBootApplication
 class EchoApplication {
 
+	static {
+		def builder = AWSXRayRecorderBuilder.standard()
+				                            .withPlugin( new EC2Plugin() )
+				                            .withPlugin( new ECSPlugin() )
+
+		def ruleFile = EchoApplication.getResource( '/sampling-rules.yml' )
+		builder.withSamplingStrategy( new LocalizedSamplingStrategy( ruleFile ) )
+		AWSXRay.setGlobalRecorder( builder.build() )
+	}
+
 	static void main(String[] args) {
 		SpringApplication.run EchoApplication, args
+	}
+
+	@Bean
+	Filter TracingFilter() {
+		new AWSXRayServletFilter( 'echo' )
 	}
 
 	@Bean
